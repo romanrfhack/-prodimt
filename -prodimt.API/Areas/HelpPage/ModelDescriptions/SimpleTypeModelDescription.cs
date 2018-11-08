2e5024087ef9513641d8a52aaa477599c4c346f8
Román Romero <roman.romero@halsoft.com.mx>
2018-11-08 16:02:41 -0600

    Creación del projecto

---
+++ -prodimt.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _prodimt.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ -prodimt.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_prodimt.Backend.Startup))]
+namespace _prodimt.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
